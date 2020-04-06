36b6f540b9318f5229e85bff5582c39c10706d96
Aamir Bilvani <aamirbilwani@gatronova.com>
2020-04-06 21:05:14 +0500

    Added CNIC QR Scanning Logic

--- raashan_site/raashan_info/static/custom/js/recipient.js
+++ raashan_site/raashan_info/static/custom/js/recipient.js
@@ -55,11 +55,15 @@ $('#checkCNIC').click(function() {
                     textvalue += received.worker.user.username + " ";
                 }
 
-                textvalue += "of " + received.worker.organization.name + " ";
+                if (received.worker.organization) {
+                    textvalue += "of " + received.worker.organization.name + " ";
+                }
 
+                // format the date
                 var m = moment(received.date);
                 textvalue += "on <b>" + m.format('dddd, MMMM Do YYYY, h:mm:ss a') + "</b>.";
 
+                // set modal content and then show it
                 $('#recipientModalLabel').text('Recipient Found');
                 $('#recipientModalContent').html(textvalue);
                 $('#recipientModal').modal('show');
@@ -77,6 +81,94 @@ $('#checkCNIC').click(function() {
                 $('#checkCNIC').html('Check CNIC');
             }
         });
+    }
+});
+
+// The window for the QR Code Scanner
+var qrCodeScanner = new Html5Qrcode('qrScanner');
+
+// If QR code is found
+const qrCodeSuccessCallback = qrCodeMessage => {
+    // If QR code is in the expected format (either 25 or 26 digits), then send it to the main screen
+    if ((qrCodeMessage.length == 25 || qrCodeMessage.length == 26) && qrCodeMessage.match('^[0-9]*$')) {
+        var cnicNumber = qrCodeMessage.slice(12,25);
+        $('#cnic').val(cnicNumber);
+        $('#scannerFeedback').text('CNIC found: ' + cnicNumber);
+        setTimeout(function() {
+            $('#scannerModal').modal('hide');
+        }, 1000); 
+    }
+    // If QR code is not in correct format, show message
+    else {   
+        $('#scannerFeedback').text('Found: ' + qrCodeMessage + '. Code not read correctly or not a valid CNIC. Please try again.');
+        setTimeout(function() {
+            $('#scannerFeedback').fadeOut(function() {
+                $('#scannerFeedback').text('Scanning...').fadeIn();
+            });
+        }, 1000);
+    }
+}
+
+// If QR code not found, this is happening constantly
+const qrCodeErrorCallback = message => {    
+}
+
+// If video stream becomes unstable for some reason
+const videoErrorCallback = message => {
+    $('#scannerFeedback').text('Video Error, error = ${message}');
+}
+
+// Start the QR Scanner using the given camera
+function qrCodeScannerStart(cameraId) {
+    qrCodeScanner.start(
+        cameraId, 
+        {fps: 10},
+        qrCodeSuccessCallback,
+        qrCodeErrorCallback)
+        .catch(videoErrorCallback);
+    $('#scannerFeedback').text('Scanning...');
+}
+
+// Show the scanner modal
+$('#scannerModalShowButton').on('click', function() {
+    // First get the list of cameras
+    Html5Qrcode.getCameras().then(cameras => {
+        // If cameras are found, populate the dropdown
+        if (cameras && cameras.length) {
+            for (var i = 0; i < cameras.length; i++) {
+                var camera = cameras[i];
+                var value = camera.id;
+                var name = camera.label == null ? value : camera.label;
+                if (i == 0) {
+                    $("#selectCamera").append(`<option value="${value}">${name}</option>`);
+                }
+            }
+            // set the on change event of the dropdown to stop the current camera and start the next camera
+            $('#selectCamera').on('change', function() {
+                qrCodeScanner.stop().then(ignore => {
+                    var cameraId = $('#selectCamera').val();
+                    qrCodeScannerStart(cameraId);
+                })
+            });
+
+            // start the first camera
+            firstCameraId = $('#selectCamera').val();
+            qrCodeScannerStart(firstCameraId);
+        } else {
+            $('#scannerFeedback').text('No cameras found on this device.');
+        }
+
+    })
+    $('#scannerModal').modal('show');
+})
+
+// on modal hide, stop the camera and destroy the dropdown entries to ensure cleanup.
+$('#scannerModal').on('hide.bs.modal', function(e) {
+    if (qrCodeScanner) {
+        qrCodeScanner.stop().then(ignore => {
+            $('#selectCamera').off('change')
+            $("#selectCamera").html('');
+        })
     }
 });
 
